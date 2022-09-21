75ad7402bc469c2e689605e85ed0ce5b433f11c7
Alex Yegupov <yegupov@gmail.com>
2022-09-21 20:38:21 +0300

    simplified interface

--- src/App2.js
+++ src/App2.js
@@ -11,12 +11,9 @@ function App2() {
   const external = document.getElementById('external2')
   return ReactDOM.createPortal(
     (<div className={`$[styles.App} ${styles.lightblue}`}>
-      <h2>React app 2 -> external2 </h2>
-      <List colorClass={styles.red} />
-      <br />
-      <button onClick={() => window._test()}>Call window._test() function</button>
-
-      </div>
+        <h2>React app 2</h2>
+        <List colorClass={styles.red} />
+     </div>
     ),
     external
   );
--- src/App3.js
+++ src/App3.js
@@ -6,7 +6,7 @@ function App3() {
   const external = document.getElementById('external2')
   return ReactDOM.createPortal(
     (<div>
-       <h2>React app 3 -> external2 </h2>
+       <h2>React app 3</h2>
      </div>
     ),
     external
